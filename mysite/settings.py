c55148ed1016d61669d61049970504cb72389ff3
Jun Hu <wuorsut@gmail.com>
2022-04-25 18:31:49 +0800

    Disable DEBUG

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECRET_KEY = os.environ.get("SECRET_KEY", '%=2qos%!(bn#r@u@%xpfr15t!bnvabmj2(d$#r-k!yt@zfq^va')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["dj-polls-app.herokuapp.com", "127.0.0.1"]
 
