ac530685d2496710705434374d69f62819bc2cf6
Jun Hu <wuorsut@gmail.com>
2022-04-27 22:21:05 +0800

    tweak the admin site stuffs

--- polls/admin.py
+++ polls/admin.py
@@ -9,6 +9,9 @@ class ChoiceInline(admin.StackedInline):
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
