e3816fae97fdef3ea0885daea12ebdbbf9ea08ee
George Hu <wuorsut@gmail.com>
2022-12-23 01:19:13 +0800

    Only user can vote, Make login-logout better

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth.decorators import login_required, permission_required
 
 app_name = 'polls'
 urlpatterns = [
@@ -11,5 +12,5 @@ urlpatterns = [
     # ex: /polls/5/results/
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/vote/', login_required(views.vote), name='vote'),
 ]
